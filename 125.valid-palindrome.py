bbc395a4fc7b25e8041c534a41895b84036b6bd4
Hal <halcorder@gmail.com>
2023-06-06 11:15:37 +0900

    valid-palindrome algorithm opt

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -10,41 +10,43 @@
 ★実装方針
 
 ●isPalindrome
-1.s = convertStr()
-2.sの先頭と末尾からそれぞれの文字が同一かどうかチェック
-    2-1.異なる場合
-        2-1-1.falseを返却して終了
-3.この段階まで来たらpalindromeであることが確定する為true返却して終了
-
-●convertStr
-1.返却用の空文字列(converted_str)を用意
-2.forを使ってstrの各文字(char)を探索する
-    2-1.charが文字or数字ではない場合
-        2-1-1.スキップ
-    2-2.coverted_strにcharを追加
-3.converted_strを返却して終了
+1.sの先頭及び最後から中央に向かって以下処理を実施
+    1-1.先頭文字(left_char)が文字・数字では無い場合
+        1-1-1.先頭文字インデックス(left_index)を増やしスキップ
+    
+    1-2.末尾文字(right_char)が文字・数字では無い場合
+        1-2-1.末尾文字インデックス(right_index)を増やしスキップ
+    
+    1-3.left_char及びright_charを小文字に変換(lowered_left_char / lowered_right_char)
+    1-4.lowered_left_charとlowered_right_charを比較
+        1-4-1.異なる場合はis_palindromeをfalseにしてbreak
+        1-4-2.同一の場合はleft_index及びright_indexを1増減させ次へ移行
+2.is_palindromeを返却して終了
 '''
 class Solution:
-    def __convertStr(self, src: str) -> str:
-        converted_str = []
-        for char in src:
-            if char.isalnum():
-                converted_str.append(char.lower())
-        return ''.join(converted_str)
-    
     def isPalindrome(self, s: str) -> bool:
-        s = self.__convertStr(s)
         is_palindrome = True
-        start_index = 0
-        end_index = len(s) - 1
+        left_index = 0
+        right_index = len(s) - 1
         
-        while start_index < end_index:
-            if s[start_index] != s[end_index]:
+        while left_index < right_index:
+            if not s[left_index].isalnum():
+                left_index += 1
+                continue
+            
+            if not s[right_index].isalnum():
+                right_index -= 1
+                continue
+            
+            lowered_left_char = s[left_index].lower()
+            lowered_right_char = s[right_index].lower()
+            
+            if lowered_left_char != lowered_right_char:
                 is_palindrome = False
                 break
             
-            start_index += 1
-            end_index -= 1
+            left_index += 1
+            right_index -= 1
             
         return is_palindrome
 
