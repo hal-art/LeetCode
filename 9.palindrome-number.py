f3b8cdaa93ea2425f28d98ac4154e1f0071ff07e
Hal <halcorder@gmail.com>
2023-06-07 11:22:22 +0900

    palindrome-number resolve

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -7,5 +7,19 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return
+        if x < 0:
+            return False
+        
+        value: str = str(x)
+        left_index = 0
+        right_index = len(value) - 1
+        
+        while left_index < right_index:
+            if value[left_index] != value[right_index]:
+                return False
+            
+            left_index += 1
+            right_index -= 1
+            
+        return True
 # @lc code=end
