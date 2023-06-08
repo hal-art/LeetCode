ce31cb937ebf1bd9a1b3aaebdbba1b9d4136ec67
hal-art <halcorder@gmail.com>
2023-06-08 22:20:16 +0900

    longest-common-prefix add comment

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -5,6 +5,22 @@
 #
 
 # @lc code=start
+'''
+○実装方針
+
+★longestCommonPrefixについて
+1.初期状態のlcpはstrs[0]とする
+2. strsの先頭 ~ strsの最後までの各文字列strに対し以下の行為を行う
+    2-1.__getLCPByTwoStrsを呼ぶ
+3.lcpを返却
+    
+★__getLCPByTwoStrsについて
+1.引数で得たstr1とstr2に対し以下の操作を行う
+2.それぞれ先頭から一文字(char)ずつ確認する
+    2-1.異なる場合は終了
+    2-2.同じ場合はlcpにcharを追加する
+3.lcpを返却
+'''
 class Solution:
     def longestCommonPrefix(self, strs) -> str:
         lcp = ""
