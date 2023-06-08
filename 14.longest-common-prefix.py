59773249e14d780a752e700b8435910a7243cdaf
hal-art <halcorder@gmail.com>
2023-06-08 22:13:54 +0900

    longetst-common-prefix solve

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -6,8 +6,28 @@
 
 # @lc code=start
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        return
+    def longestCommonPrefix(self, strs) -> str:
+        lcp = ""
+        lcp = strs[0]
+        for i, str in enumerate(strs):
+            if i == 0:
+                continue
+            
+            lcp = self.__getLCPByTwoStrs(lcp, str)
+            if lcp == '':
+                break
         
+        return lcp
+        
+    def __getLCPByTwoStrs(self, str1:  str, str2: str) -> str:
+        length = min(len(str1), len(str2)) 
+        lcp = ""
+
+        for i in range(length):  # 短い文字列の長さだけループを回す
+            if str1[i] != str2[i]:
+                break
+            lcp += str1[i]
+
+        return lcp
 # @lc code=end
 
