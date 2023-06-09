7fb6d7d76e1ac8920fb336a7290662f629229f96
hal-art <halcorder@gmail.com>
2023-06-10 01:17:17 +0900

    binary-tree-preorder-traversal solve

--- 144.binary-tree-preorder-traversal.py
+++ 144.binary-tree-preorder-traversal.py
@@ -5,14 +5,46 @@
 #
 
 # @lc code=start
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+'''
+○実装方針
+1.rootノードをenqueする
+2.以下を i(=1) < len(root)の間行う
+    2-1.dequeして現在のノード情報を取り出す
+    2-1.root[i]があるか確認する
+        2-1-1.ある場合はNode(root[i])をし左子ノードを作成する
+        2-1-2.enqueを行い、左子ノードを検査対象に含める
+        2-1-3.i++
+        
+    2-2.root[i+1]があるか確認する
+        2-2-1.ある場合はNode(root[i+1])をし右子ノードを作成する
+        2-2-2.enqueを行い、右子ノードを検査対象に含める
+        2-2-3.i++
+'''
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return
+    def preorderTraversal(self, tree: TreeNode):
+        if tree is None:
+            return []
+        
+        outList: list[int] = []
+        stack: list[TreeNode] = [tree]
+        
+        while stack:
+            currentNode = stack.pop()
+            outList.append(currentNode.val)
+            
+            if not currentNode.right is None:
+                stack.append(currentNode.right)
+                
+            if not currentNode.left is None:
+                stack.append(currentNode.left)
+        return outList
+#obj = Solution()
+#obj.preorderTraversal([1,None,2,3])
 # @lc code=end
 
