e5ba255d5c0941d46d57394652bbf171198a9e1f
Hal <halcorder@gmail.com>
2023-06-14 10:37:17 +0900

    reverse string solve

--- 344.reverse-string.py
+++ 344.reverse-string.py
@@ -7,4 +7,14 @@
 # @lc code=start
 class Solution:
     def reverseString(self, s) -> None:
+        leftIndex = 0
+        rightIndex = len(s) - 1 - leftIndex
+        
+        while leftIndex < rightIndex:
+            tmp  = s[leftIndex]
+            s[leftIndex] = s[rightIndex]
+            s[rightIndex] = tmp
+            
+            leftIndex += 1
+            rightIndex -= 1
 # @lc code=end
